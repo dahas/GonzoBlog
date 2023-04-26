ef99cdaed767f67cdbc70352acb2038a788cd109
dahas <martin.j.wolf@gmail.com>
2023-04-26 12:32:55 +0200

    Deletion Needs A Confirmation Now

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,11 +1,14 @@
 
+// Usage: ajax.get(<url>);
 const ajax = {
     get: url => {
         const xhr = new XMLHttpRequest();
         xhr.responseType = "json";
+        // Success:
         xhr.addEventListener("load", ev => {
             console.log(ev.currentTarget.response);
         });
+        // Error:
         xhr.addEventListener("error", ev => {
             console.log("Request failed!");
         });
@@ -14,3 +17,27 @@ const ajax = {
     }
 };
 
+// Modal Popup On Delete
+const deleteModal = document.getElementById('confirmDelete');
+
+deleteModal.addEventListener('show.bs.modal', function (event) {
+    const elm = event.relatedTarget;
+
+    document.getElementById('confirmDeleteLabel').innerText = "Really delete this " + ucfirst(elm.dataset.ctype) + "?";
+
+    const confirmButton = document.getElementById('confirmDeleteButton');
+    confirmButton.addEventListener("click", () => {
+        location.href = elm.href;
+    });
+});
+
+// ----- Helper ----------------------
+
+/**
+ * Make first letter uppercase
+ * @param {string} str 
+ * @returns string
+ */
+function ucfirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
